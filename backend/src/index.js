10902c29369cf9150dc3ff5afb06fafe7f55ecdf
Veer Sandhu <sandhu.s.veer@gmail.com>
2025-01-15 22:40:43 -0500

    push night

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.routes.js";
+import {connectDB} from './lib/db.js';
 
 const app = express();
 
@@ -11,4 +12,6 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log('server is running on PORT: ' + PORT);
+    connectDB();
 });
+
