b7a5f17eafa589461a1d52289d05e73d3be52d07
Veer Sandhu <sandhu.s.veer@gmail.com>
2025-01-19 14:40:11 -0500

    backend shell done (socketio todo)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
 import {connectDB} from './lib/db.js';
 import cookieParser from "cookie-parser";
 
@@ -11,10 +13,11 @@ const PORT = process.env.PORT;
 
 // let you extract json data out of body
 app.use(express.json())
-
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.listen(PORT, () => {
     console.log('server is running on PORT: ' + PORT);
