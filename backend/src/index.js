f4b27bf8455c613743ffc92aa59ee9010221f36e
Veer Sandhu <sandhu.s.veer@gmail.com>
2025-01-19 14:05:36 -0500

    auth big progress

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.routes.js";
 import {connectDB} from './lib/db.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT;
 
 // let you extract json data out of body
 app.use(express.json())
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
 
