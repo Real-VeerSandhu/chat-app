a432ee332a928ed4a5daf92870c3fdc2633343a5
Veer Sandhu <sandhu.s.veer@gmail.com>
2025-01-19 16:43:19 -0500

    signup done

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import {create} from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 
+// *axios is state management*
 export const useAuthStore = create((set) => ({
     authUser: null,
     isSigningUp: false,
@@ -22,6 +23,26 @@ export const useAuthStore = create((set) => ({
     },
 
     signup: async(data) => {
-        
-    }
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
+
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+          await axiosInstance.post("/auth/logout");
+          set({ authUser: null });
+          toast.success("Logged out successfully");
+        } catch (error) {
+          toast.error(error.response.data.message);
+        }
+      },
 }));
